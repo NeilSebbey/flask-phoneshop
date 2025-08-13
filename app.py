2b8efa5e98bbfea0d0674d4299ed61b00e910285
Neil Sebbey <neil@sebbey.com>
2025-08-13 15:12:43 +0100

    Updated dependencies and added Gunicorn support.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask
 from extensions import pdb
+from dotenv import load_dotenv
+load_dotenv(".env.local")
 
 # environmental variables for DB connection and secrets
 DATABASE_HOST = os.getenv('DATABASE_HOST')
@@ -8,7 +10,6 @@ DATABASE_USERNAME = os.getenv('DATABASE_USERNAME')
 DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
 DATABASE_PORT = os.getenv('DATABASE_PORT')
 DATABASE_NAME = os.getenv('DATABASE_NAME')
-SECRET_KEY = os.getenv('SECRET_KEY')
 
 # create and configure the app
 app = Flask(__name__)
@@ -16,7 +17,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = ('postgresql://' + DATABASE_USERNAME + ':' + DATABASE_PASSWORD +
                                          '@' + DATABASE_HOST + ':' + DATABASE_PORT + '/' + DATABASE_NAME)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = SECRET_KEY
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 pdb.init_app(app)
 
